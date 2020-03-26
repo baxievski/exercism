da7343c776a06fb75a03d1dbfa71c069af5c1419
Bojan Adzievski <bojana@tabtale.com>
2020-03-26 19:17:36 +0100

    add print() for debug

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,5 +1,9 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 def add(birth_date, seconds_to_add=10**9):
     return birth_date + timedelta(seconds=seconds_to_add)
+
+
+if __name__ == "__main__":
+    print(f"{add(datetime(1979, 10, 18))=}")
--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,6 +2,9 @@ class Matrix(object):
     def __init__(self, matrix):
         self.matrix = [[int(c) for c in r.split()] for r in matrix.split('\n')]
 
+    def __repr__(self):
+        return str(self.matrix)
+
     def row(self, i):
         return self.matrix[i-1]
     
@@ -11,4 +14,5 @@ class Matrix(object):
 
 if __name__ == '__main__':
     test_matrix = Matrix("9 8 7\n5 3 2\n 6 6 7")
-    print(test_matrix.column(1))
+    print(f"{test_matrix=}")
+    print(f"{test_matrix.column(1)=}")
