3443c2f94a2a23a7563e28011c35f9d561ff56ef
Bojan Adzievski <bojana@tabtale.com>
2020-03-26 19:34:24 +0100

    add isogram solution

---
+++ python/isogram/isogram.py
@@ -0,0 +1,24 @@
+import re
+from collections import defaultdict
+
+
+def is_isogram(string_to_check):
+    character_counts = defaultdict(int)
+    string_to_check = re.sub(
+        r'\s+|\-',
+        '',
+        string_to_check,
+        flags=re.UNICODE
+    )
+    string_to_check = string_to_check.lower()
+    for character in string_to_check:
+        character_counts[character] += 1
+        if character_counts[character] <= 1:
+            continue
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    print(f'{is_isogram("bojan")=}')
+    print(f'{is_isogram("axievski")=}')
