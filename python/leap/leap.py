8b732fe87bad9d7f016f55fa1e7355f83b5de527
Bojan Adzievski <bojana@tabtale.com>
2020-03-26 19:18:04 +0100

    flattent nested if

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,16 +1,14 @@
 def leap_year(year):
     if year % 4 != 0:
         return False
-
-    if year % 100 == 0:
-        if year % 400 == 0:
-            return True
-        return False
-
-    return True
+    if year % 100 != 0:
+        return True
+    if year % 400 == 0:
+        return True
+    return False
 
 if __name__ == "__main__":
-    print(f"1997: {leap_year(1997)}")
-    print(f"1996: {leap_year(1996)}")
-    print(f"1900: {leap_year(1900)}")
-    print(f"2000: {leap_year(2000)}")
+    print(f"{leap_year(1997)=}")
+    print(f"{leap_year(1996)=}")
+    print(f"{leap_year(1900)=}")
+    print(f"{leap_year(2000)=}")
