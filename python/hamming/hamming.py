f3877a0334a7445b5c071888d12a456edd3fb3b8
Bojan Adzievski <bojana@tabtale.com>
2020-03-26 19:33:02 +0100

    add hamming solu

---
+++ python/hamming/hamming.py
@@ -0,0 +1,14 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError('strand_a, strand_b must have the same length')
+    result = 0
+    for i, c_str_a in enumerate(strand_a):
+        if c_str_a != strand_b[i]:
+            result += 1
+    return result
+
+
+if __name__ == "__main__":
+    print(f"{distance('AABB', 'AABC')=}")
+    print(f"{distance('AACB', 'AABC')=}")
+    print(f"{distance('AACB', 'AAC')=}")
