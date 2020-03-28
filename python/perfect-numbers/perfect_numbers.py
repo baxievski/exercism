c5be3e7c159966ad2f3a80d84eff17b4e9d25967
Bojan Adzievski <bojana@tabtale.com>
2020-03-28 20:17:08 +0100

    update perfect-numbers python solution

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -3,16 +3,26 @@ import math
 
 def classify(number):
     if number <= 0:
-        raise ValueError('Only positive numbers can be classified')
-    factors = sum([[i, number // i] for i in range(1, math.ceil(number ** 0.5)) if number % i == 0], [])
+        raise ValueError("Only positive numbers can be classified")
+    factors = sum(
+        [
+            [i, number // i]
+            for i in range(1, math.ceil(number ** 0.5))
+            if number % i == 0
+        ],
+        [],
+    )
     if sum(factors) == 2 * number:
-        result = 'perfect'
-    elif sum(factors) > 2 * number:
-        result = 'abundant'
-    elif sum(factors) < 2 * number:
-        result = 'deficient'
-    return result
+        return "perfect"
+    if sum(factors) > 2 * number:
+        return "abundant"
+    return "deficient"
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    print(f"{classify(6)=}")
+    print(f"{classify(8)=}")
+    print(f"{classify(9)=}")
+    print(f"{classify(10)=}")
+    print(f"{classify(11)=}")
+    print(f"{classify(12)=}")
