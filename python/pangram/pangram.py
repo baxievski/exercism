7e097a492fad716083d99145f3639ac04643048e
Bojan Adzievski <bojana@tabtale.com>
2020-03-28 10:48:05 +0100

    update pangram python solution

---
+++ python/pangram/pangram.py
@@ -0,0 +1,10 @@
+import string
+
+
+def is_pangram(sentence):
+    alphabet = set(string.ascii_lowercase)
+    return alphabet.issubset(set(sentence.lower()))
+
+if __name__ == "__main__":
+    print(f"{is_pangram('abcdef')=}")
+    print(f"{is_pangram('abcdefghijklmnopqrstuvwxyz')=}")
