57e3971bd8ee96518c2137b5dd920a32c618fc0e
Bojan Adzievski <bojana@tabtale.com>
2020-04-04 23:04:51 +0200

    fix formatting

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,24 +1,24 @@
 class Allergies(object):
-    alergies_map = {
-        1: 'eggs',
-        2: 'peanuts',
-        4: 'shellfish',
-        8: 'strawberries',
-        16: 'tomatoes',
-        32: 'chocolate',
-        64: 'pollen',
-        128: 'cats',
+    names = {
+        1: "eggs",
+        2: "peanuts",
+        4: "shellfish",
+        8: "strawberries",
+        16: "tomatoes",
+        32: "chocolate",
+        64: "pollen",
+        128: "cats",
     }
 
     def __init__(self, score):
         _score = score % 256
-        self.allergies = [Allergies.alergies_map[i] for i in (2**x for x in range(8)) if i & _score]
+        self.lst = [
+            Allergies.names[i]
+            for i in (2 ** x for x in range(8))
+            if i & _score
+        ]
 
     def allergic_to(self, item):
-        if item in self.allergies:
+        if item in self.lst:
             return True
         return False
-
-    @property
-    def lst(self):
-        return self.allergies
