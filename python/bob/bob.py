956c4f2f115388b4bd1b5ca0c955af35643ab716
Bojan Adzievski <bojana@tabtale.com>
2020-04-04 21:57:25 +0200

    add bob python solution

---
+++ python/bob/bob.py
@@ -0,0 +1,15 @@
+def response(phrase):
+    phrase = phrase.strip()
+    if not phrase:
+        return 'Fine. Be that way!'
+    if phrase.isupper() and phrase.endswith("?"):
+        return "Calm down, I know what I'm doing!"
+    if phrase.isupper():
+        return 'Whoa, chill out!'
+    if phrase.endswith('?'):
+        return 'Sure.'
+    return 'Whatever.'
+
+
+if __name__ == "__main__":
+    print(f"{response()=}")
