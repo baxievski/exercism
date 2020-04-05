93651c7a5061c962f42d365481cd6d5a2319b07d
Bojan Adzievski <bojana@tabtale.com>
2020-04-05 14:46:11 +0200

    add sieve python solution

---
+++ python/sieve/sieve.py
@@ -0,0 +1,14 @@
+def primes(limit):
+    primes = [True if x >= 2 else False for x in range(limit+1)]
+    result = []
+    for i, prime in enumerate(primes):
+        if not prime:
+            continue
+        result.append(i)
+        for j in range(i, limit+1, i):
+            primes[j] = False
+    return result
+
+
+if __name__ == "__main__":
+    print(f"{primes(100)=}")
