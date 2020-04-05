944d5fe909377b1ee7ab3ac8072d514120290492
Bojan Adzievski <bojana@tabtale.com>
2020-04-05 12:14:00 +0200

    add meetup python solution

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,33 +1,34 @@
 from datetime import date, timedelta
 
+
 class MeetupDayException(Exception):
     pass
 
 
 def meetup(year, month, week, day_of_the_week):
     day_map = {
-        'Monday': 0,
-        'Tuesday': 1,
-        'Wednesday': 2,
-        'Thursday': 3,
-        'Friday': 4,
-        'Saturday': 5,
-        'Sunday': 6,
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6,
     }
     week_map = {
-        '1st': 0,
-        '2nd': 1,
-        '3rd': 2,
-        '4th': 3,
-        '5th': 4,
-        'last': -1,
+        "1st": 0,
+        "2nd": 1,
+        "3rd": 2,
+        "4th": 3,
+        "5th": 4,
+        "last": -1,
     }
     days = [
         day
         for day in (date(year, month, 1) + timedelta(days=x) for x in range(32))
         if day.weekday() == day_map[day_of_the_week] and day.month == month
     ]
-    if week == 'teenth':
+    if week == "teenth":
         for day in days:
             if 12 < day.day < 20:
                 return day
