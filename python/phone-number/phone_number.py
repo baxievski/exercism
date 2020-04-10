b65cc819c72455e1afcabba17a28b845e11c4c19
Bojan Adzievski <bojana@tabtale.com>
2020-04-10 21:53:55 +0200

    wip

---
+++ python/phone-number/phone_number.py
@@ -0,0 +1,58 @@
+import re
+
+
+class PhoneNumber(object):
+    def __init__(self, phone_number):
+        number = re.sub(r'[\(\)\.\-\+\s]', '', phone_number)
+        self._number = number
+        if not self._validate():
+            raise ValueError(f"'{phone_number}' - not a valid north american phone number.")
+
+    def _validate(self):
+        if len(self._number) > 11:
+            return False
+        if len(self._number) == 11 and not self._number[0] == "1":
+            return False
+        if not self._number.isdigit():
+            return False
+        if self.area_code[0] in "01":
+            return False
+        if self.exchange_code[0] in "01":
+            return False
+        return True
+
+    @property
+    def area_code(self):
+        n = self._number
+        if len(n) == 11:
+            n = n[1:]
+        return n[:3]
+
+    @property
+    def exchange_code(self):
+        n = self._number
+        if len(n) == 11:
+            n = n[1:]
+        return n[3:6]
+
+    @property
+    def subscriber_number(self):
+        n = self._number
+        if len(n) == 11:
+            n = n[1:]
+        return n[6:]
+
+    @property
+    def number(self):
+        return self.area_code + self.exchange_code + self.subscriber_number
+
+    def pretty(self):
+        p_format = '({area_code}) {exchange_code}-{subscriber_number}'
+        return p_format.format(
+            area_code=self.area_code,
+            exchange_code=self.exchange_code,
+            subscriber_number=self.subscriber_number)
+
+
+if __name__ == "__main__":
+    pass
