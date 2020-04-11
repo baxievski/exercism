3e43b3d8f7ddadd3cea03df28a2db6fe3424e979
Bojan Adzievski <bojana@tabtale.com>
2020-04-11 10:35:18 +0200

    add series python solution

---
+++ python/series/series.py
@@ -0,0 +1,16 @@
+def slices(series, length):
+    if length > len(series):
+        raise ValueError("Slice length is larger than the series length.")
+    if not length:
+        raise ValueError("Slice length can not be 0.")
+    if length < 0:
+        raise ValueError("Slice length can not be negative.")
+    result = [
+        "".join(series[i:i+length])
+        for i in range(len(series) - length + 1)
+    ]
+    return result
+
+
+if __name__ == "__main__":
+    print(f'{slices("123456789", 3)=}')
