f751457e5a114a681573caad31f4cd8b6da1eaa8
Bojan Adzievski <bojana@tabtale.com>
2020-05-06 22:30:21 +0200

    add two-fer go solution

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,12 @@
+// Package twofer implements the Exercism "two-fer" exercise.
+package twofer
+
+import "fmt"
+
+// ShareWith returns a string in the form "One for you, one for me."
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %v, one for me.", name)
+}
