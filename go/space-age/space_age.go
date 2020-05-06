d01598abdc81f05fd670419b8de1db184dec2c3a
Bojan Adzievski <bojana@tabtale.com>
2020-05-06 21:36:24 +0200

    update space-age go solution

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,25 +3,16 @@ package space
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	earthYears := seconds / 31557600.0;
-	switch planet {
-		case "Mercury":
-			return earthYears / 0.2408467;
-		case "Venus":
-			return earthYears / 0.61519726;
-		case "Earth":
-			return earthYears;
-		case "Mars":
-			return earthYears / 1.8808158;
-		case "Jupiter":
-			return earthYears / 11.862615;
-		case "Saturn":
-			return earthYears / 29.447498;
-		case "Uranus":
-			return earthYears / 84.016846;
-		case "Neptune":
-			return earthYears / 164.79132;
-		default:
-			return earthYears;
+	earthSeconds := 31557600.0;
+	periods := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1.0,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
+	return seconds / earthSeconds / periods[planet];
 }
