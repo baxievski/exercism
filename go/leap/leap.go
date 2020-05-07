e6d74769ca977fa47758b937f9678204168ced63
Bojan Adzievski <bojana@tabtale.com>
2020-05-07 06:32:56 +0200

    add leap go solution

---
+++ go/leap/leap.go
@@ -0,0 +1,17 @@
+// Package leap implements exercism leap year solution
+// https://golang.org/doc/effective_go.html#commentary
+package leap
+
+// IsLeapYear returns true if the year passed as arg is a leap year
+func IsLeapYear(year int) bool {
+	if year%4 != 0 {
+		return false
+	}
+	if year%100 != 0 {
+		return true
+	}
+	if year%400 == 0 {
+		return true
+	}
+	return false
+}
