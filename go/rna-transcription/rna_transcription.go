8e177fbb6904ddd847acb0c63b0530beb8854bde
Bojan Adzievski <bojana@tabtale.com>
2020-05-08 17:25:03 +0200

    add rna-transcription go solution

---
+++ go/rna-transcription/rna_transcription.go
@@ -0,0 +1,17 @@
+// Package strand implements a solution for the exercism rna-transcription chalenge
+package strand
+
+// ToRNA translates a DNA string to RNA
+func ToRNA(dna string) string {
+	dnaToRna := map[rune]rune{
+		'G': 'C',
+		'C': 'G',
+		'T': 'A',
+		'A': 'U',
+	}
+	rna := []rune{}
+	for _, nucleotide := range dna {
+		rna = append(rna, dnaToRna[nucleotide])
+	}
+	return string(rna)
+}
