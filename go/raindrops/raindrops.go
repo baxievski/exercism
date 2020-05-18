7c67986afc2e5b8c061a5fd6aae4b00857c8ab1f
Bojan Adzievski <bojana@tabtale.com>
2020-05-18 20:13:37 +0200

    update raindrops go solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -9,14 +9,13 @@ import (
 // Convert returns the "raindrop" string corresponding to the factors of the integer arg
 func Convert(num int) string {
 	result := []string{}
-	f := factors(num)
-	if elemInSlice(3, f) {
+	if num%3 == 0 {
 		result = append(result, "Pling")
 	}
-	if elemInSlice(5, f) {
+	if num%5 == 0 {
 		result = append(result, "Plang")
 	}
-	if elemInSlice(7, f) {
+	if num%7 == 0 {
 		result = append(result, "Plong")
 	}
 	if len(result) == 0 {
@@ -24,30 +23,4 @@ func Convert(num int) string {
 	}
 	return strings.Join(result, "")
 
-}
-
-func factors(num int) []int {
-	result := []int{}
-	counter := 2
-	for counter*counter <= num {
-		if num%counter != 0 {
-			counter++
-			continue
-		}
-		num = num / counter
-		result = append(result, counter)
-	}
-	if num > 1 {
-		result = append(result, num)
-	}
-	return result
-}
-
-func elemInSlice(a int, list []int) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
 }
