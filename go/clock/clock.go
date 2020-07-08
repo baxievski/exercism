9a5c713944e9a8f1ff8626700e651e0532cf26b3
Bojan Adzievski <bojana@tabtale.com>
2020-07-08 21:11:25 +0200

    update go/clock - remove Time.normalize()

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -8,29 +8,29 @@ type Time struct {
 	hours, minutes int
 }
 
-func (t *Time) normalize() {
-	t.hours = (t.hours + t.minutes/60) % 24
-	t.minutes = t.minutes % 60
+// New generates the time
+func New(hours, minutes int) Time {
+	hours = (hours + minutes/60) % 24
+	minutes = minutes % 60
 
-	if t.minutes < 0 {
-		t.minutes += 60
-		t.hours--
+	if minutes < 0 {
+		minutes += 60
+		hours--
 	}
 
-	for t.hours < 0 {
-		t.hours += 24
+	for hours < 0 {
+		hours += 24
 	}
-}
 
-func (t Time) String() string {
-	return fmt.Sprintf("%02d:%02d", t.hours, t.minutes)
+	return Time{
+		hours:   hours,
+		minutes: minutes,
+	}
 }
 
 // Add gives the time after m number of minutes
 func (t Time) Add(m int) Time {
-	t.minutes += m
-	t.normalize()
-	return t
+	return New(t.hours, t.minutes+m)
 }
 
 // Subtract gives the time before m number of minutes
@@ -38,12 +38,6 @@ func (t Time) Subtract(m int) Time {
 	return t.Add(-1 * m)
 }
 
-// New generates the time
-func New(h, m int) Time {
-	t := Time{
-		hours:   h,
-		minutes: m,
-	}
-	t.normalize()
-	return t
+func (t Time) String() string {
+	return fmt.Sprintf("%02d:%02d", t.hours, t.minutes)
 }
