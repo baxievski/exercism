1af5b84e65cca4e2568f673ef345e9cd0b6ead8f
Bojan Adzievski <bojana@tabtale.com>
2020-07-20 06:43:34 +0200

    update parallel letter frequency go solution

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,32 +1,29 @@
+// Package letter implements a solution for the paralllel-letter-frequency exercism problem
 package letter
 
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
-// ConcurrentFrequency counts the frequency of each rune in a given list of strings
+// ConcurrentFrequency concurrently counts the frequency of each rune in a given list of strings
 func ConcurrentFrequency(l []string) FreqMap {
 	result := FreqMap{}
 	f := make(chan FreqMap, len(l))
-
 	for _, s := range l {
 		go cFrequency(s, f)
 	}
-
 	for range l {
 		for r, c := range <-f {
 			result[r] += c
 		}
 	}
-
 	return result
 }
 
-func cFrequency(s string, c chan FreqMap) {
+func cFrequency(s string, c chan<- FreqMap) {
 	c <- Frequency(s)
 }
 
-// Frequency counts the frequency of each rune in a given text and returns this
-// data as a FreqMap.
+// Frequency counts the frequency of each rune in a given string
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
 	for _, r := range s {
--- go/parallel-letter-frequency/parallel_letter_frequency_test.go
+++ go/parallel-letter-frequency/parallel_letter_frequency_test.go
@@ -38,6 +38,46 @@ And the rockets' red glare, the bombs bursting in air,
 Gave proof through the night that our flag was still there;
 O say does that star-spangled banner yet wave,
 O'er the land of the free and the home of the brave?`
+
+	newsMk = `До 1 ноември во Македонија ќе починат 1.629 луѓе од
+КОВИД-19 ако продолжи олеснувањето на мерките за заштита сè
+додека дневната бројка на мртви не достигне 8 на 1.000.000 жители,
+кога повторно би се вовеле построги мерки, според проекцијата на
+американскиот Институт за здравствени мерења и проценки при
+Универзитетот Вашингтон од Сиетл.
+Покрај ова, институтот од Сиетл предвидува уште две сценарија.
+Најлошото сценарио е ако продолжи олеснувањето на мерките и никогаш
+не бидат вратени, и во тој случај проекцијата е 1.805 мртви до 1 ноември.
+Според најповолното сценарио, ако 95 отсто од граѓаните носат маски на
+јавни места, бројот на починати до 1 ноември би изнесувал 752.
+Според проекцијата за најлошото сценарио, бројот на починати би пораснал
+до 32 дневно до 1 ноември, додека во најповолното сценарио, таа бројка е 3 дневно.
+Проекциите на Институтот за здравствени мерења и проценки се врз основа на
+бројките до 11 јули, кога имаше 380 починати во Македонија.
+Заклучно со вчера, вкупно дијагностицирани со КОВИД-19 во земјава има 8.786,
+од кои оздравени се 4.676, починати се 406, а активни се 3.704.`
+
+	newsUs = `Protests in Portland, Ore., continued through early Sunday morning,
+following the Oregon Department of Justice's announcement it would be suing several
+federal agencies for civil rights abuses in the state. Demonstrations have taken
+place in the city for weeks following the police killing of George Floyd in May.
+Protesters dismantled fences in front of the Multnomah County Justice Center and
+Hatfield Federal Courthouse. The U.S. Attorney's Office in Oregon had previously
+said the fences were in place to "de-escalate tensions between protesters and
+federal law enforcement officers" and allow repairs on the buildings to begin.
+Demonstrators also set fire to the Portland Police Association building,
+according to Portland police. The fire was put out later in the evening,
+and police declared a riot in the area.
+Tear gas and flash bangs were used on protesters and arrests were made,
+according to videos and photos from the scene posted on social media.
+The Oregon Department of Justice announced Saturday it would be suing several
+federal agencies for civil rights abuses, and state prosecutors will potentially
+pursue criminal charges against a federal officer who seriously injured a protester.
+The federal lawsuit names the U.S. Department of Homeland Security, the
+U.S. Marshals Service, the United States Customs and Border Protection,
+and the Federal Protective Service, agencies that have had a role in stepped-up
+force used against protesters since early July.
+The state filed the lawsuit late Friday night.`
 )
 
 func OriginalFrequency(s string) FreqMap {
@@ -49,8 +89,8 @@ func OriginalFrequency(s string) FreqMap {
 }
 
 func TestConcurrentFrequency(t *testing.T) {
-	seq := OriginalFrequency(euro + dutch + us)
-	con := ConcurrentFrequency([]string{euro, dutch, us})
+	seq := OriginalFrequency(euro + dutch + us + newsMk + newsUs)
+	con := ConcurrentFrequency([]string{euro, dutch, us, newsMk, newsUs})
 	if !reflect.DeepEqual(con, seq) {
 		t.Fatal("ConcurrentFrequency wrong result")
 	}
