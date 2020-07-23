250c0babbfa3439d06f6eb792b4c51377b3c0785
Bojan Adzievski <bojana@tabtale.com>
2020-07-23 19:42:57 +0200

    update matching-brackets go solution

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -4,48 +4,24 @@ package brackets
 // Bracket tells if the brackets are matched
 func Bracket(brackets string) bool {
 	stack := []rune{}
-	openBrackets := map[rune]bool{
-		'{': true,
-		'[': true,
-		'(': true,
-	}
 	cloosedToOpen := map[rune]rune{
 		'}': '{',
 		']': '[',
 		')': '(',
 	}
-	for _, b := range clean(brackets) {
-		if openBrackets[b] {
+	for _, b := range brackets {
+		switch b {
+		case '{', '[', '(':
 			stack = append(stack, b)
-			continue
-		}
-		if len(stack) == 0 {
-			return false
-		}
-		lastBrace := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-		openingBrace := cloosedToOpen[b]
-		if lastBrace != openingBrace {
-			return false
+		case '}', ']', ')':
+			if len(stack) == 0 {
+				return false
+			}
+			if stack[len(stack)-1] != cloosedToOpen[b] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
 		}
 	}
 	return len(stack) == 0
-}
-
-func clean(braces string) string {
-	allBraces := map[rune]bool{
-		'{': true,
-		'[': true,
-		'(': true,
-		'}': true,
-		']': true,
-		')': true,
-	}
-	cleaned := []rune{}
-	for _, v := range braces {
-		if allBraces[v] {
-			cleaned = append(cleaned, v)
-		}
-	}
-	return string(cleaned)
 }
